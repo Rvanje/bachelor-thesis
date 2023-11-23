19b421187171b9e4a1cef3c2de9d69877de290ae
Rvanje <53704685+Rvanje@users.noreply.github.com>
2023-11-23 19:45:43 +0100

    - Change the "Sign-In"-tab buttons to a good looking number field - Find out how to disable widgets

--- guic_custom.py
+++ guic_custom.py
@@ -1,6 +1,7 @@
 import tkinter
 import tkinter.messagebox
 import customtkinter
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -8,15 +9,23 @@ customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "gre
 
 class App(customtkinter.CTk):
     def __init__(self):
+        global dict_user
+        dict_user = {"999999": "Administrator",
+                     "888888": "Service Technician",
+                     "111111": "Leroy Harreh",
+                     "222222": "Tobias Graetzer",
+                     "333333": "Bojan Resan"}
         super().__init__()
 
         # configure window
         self.title("Laser Configuration Utility")
-        self.geometry(f"{1100}x{580}")
+        self.geometry(f"{1024}x{600}")
+        # self.overrideredirect(True)  # drops the taskbar on top of the window
+        # self.deactivate_automatic_dpi_awareness()
 
         # configure grid layout (4x4)
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_columnconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10), weight=1)
+        # self.grid_columnconfigure((2, 3, 4, 5, 6), weight=)
         self.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10), weight=1)
 
         # create sidebar frame with widgets
@@ -59,34 +68,53 @@ class App(customtkinter.CTk):
         self.tabview.add("Sign In")
         self.tabview.add("Main")
         self.tabview.add("Graphs")
-        self.tabview.add("Tab 4")
         self.tabview.add("Export")
-        self.tabview.tab("Main").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
-        self.tabview.tab("Sign In").grid_columnconfigure(0, weight=1)
-        # "Sign In"-tab
-        self.entry_user = customtkinter.CTkEntry(self.tabview.tab("Sign In"), placeholder_text="User")
-        self.entry_user.grid(row=5, column=0, columnspan=1, padx=(10, 0), pady=(0, 0), sticky="nsew")
-        self.entry_psw = customtkinter.CTkEntry(self.tabview.tab("Sign In"), placeholder_text="password")
-        self.entry_psw.grid(row=6, column=0, columnspan=1, padx=(10, 0), pady=(10, 0), sticky="nsew")
+        self.tabview.add("Settings")
+        self.tabview.tab("Main").grid_columnconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+                                                      weight=1)  # configure grid of individual tabs
+        self.tabview.tab("Sign In").grid_columnconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12),
+                                                         weight=1)  # configure grid of individual tabs
 
-        self.string_input_button = customtkinter.CTkButton(self.tabview.tab("Sign In"), text="Sign In",
-                                                           command=self.sign_in_button_event, anchor="center")
-        self.string_input_button.grid(row=7, column=0, padx=100, pady=(10, 10))
+        # "Sign In"-tab
+        # self.string_input_button = customtkinter.CTkButton(self.tabview.tab("Sign In"), text="Sign In",
+        #                                                    command=self.sign_in_button_event, anchor="center")
+        # self.string_input_button.grid(row=7, column=2)
+        self.string_input_button_1 = customtkinter.CTkButton(self.tabview.tab("Sign In"), text="1",
+                                                             command=self.sign_in_button_event, height=100, width=100)
+        self.string_input_button_1.grid(row=1, column=5, padx=(10, 10), pady=(10, 10))
+        self.string_input_button_2 = customtkinter.CTkButton(self.tabview.tab("Sign In"), text="2",
+                                                             command=self.sign_in_button_event, height=100, width=100)
+        self.string_input_button_2.grid(row=1, column=6, padx=(10, 10), pady=(10, 10))
+        self.string_input_button_3 = customtkinter.CTkButton(self.tabview.tab("Sign In"), text="3",
+                                                             command=self.sign_in_button_event, height=100, width=100)
+        self.string_input_button_3.grid(row=1, column=7, padx=(10, 10), pady=(10, 10))
+        self.string_input_button_4 = customtkinter.CTkButton(self.tabview.tab("Sign In"), text="4",
+                                                             command=self.sign_in_button_event, height=100, width=100)
+        self.string_input_button_4.grid(row=2, column=5, padx=(10, 10), pady=(10, 10))
+        self.string_input_button_5 = customtkinter.CTkButton(self.tabview.tab("Sign In"), text="5",
+                                                             command=self.sign_in_button_event, height=100, width=100)
+        self.string_input_button_5.grid(row=2, column=6, padx=(10, 10), pady=(10, 10))
+        self.string_input_button_6 = customtkinter.CTkButton(self.tabview.tab("Sign In"), text="6",
+                                                             command=self.sign_in_button_event, height=100, width=100)
+        self.string_input_button_6.grid(row=2, column=7, padx=(10, 10), pady=(10, 10))
+        self.string_input_button_7 = customtkinter.CTkButton(self.tabview.tab("Sign In"), text="7",
+                                                             command=self.sign_in_button_event, height=100, width=100)
+        self.string_input_button_7.grid(row=3, column=5, padx=(10, 10), pady=(10, 10))
+        self.string_input_button_8 = customtkinter.CTkButton(self.tabview.tab("Sign In"), text="8",
+                                                             command=self.sign_in_button_event, height=100, width=100)
+        self.string_input_button_8.grid(row=3, column=6, padx=(10, 10), pady=(10, 10))
+        self.string_input_button_9 = customtkinter.CTkButton(self.tabview.tab("Sign In"), text="9",
+                                                             command=self.sign_in_button_event, height=100, width=100)
+        self.string_input_button_9.grid(row=3, column=7, padx=(10, 10), pady=(10, 10))
+        self.string_input_button_0 = customtkinter.CTkButton(self.tabview.tab("Sign In"), text="0",
+                                                             command=self.sign_in_button_event, height=100, width=100)
+        self.string_input_button_0.grid(row=4, column=6, padx=(10, 10), pady=(10, 10))
 
         # create main entry and button
-        self.entry = customtkinter.CTkEntry(self.tabview.tab("Main"), placeholder_text="P Parameter")
-        self.entry.grid(row=1, column=0, columnspan=1, padx=(10, 0), pady=(0, 0), sticky="nsew")
-        self.entry = customtkinter.CTkEntry(self.tabview.tab("Main"), placeholder_text="I Parameter")
-        self.entry.grid(row=2, column=0, columnspan=1, padx=(10, 0), pady=(5, 0), sticky="nsew")
-        self.entry = customtkinter.CTkEntry(self.tabview.tab("Main"), placeholder_text="D Parameter")
-        self.entry.grid(row=3, column=0, columnspan=1, padx=(10, 0), pady=(5, 0), sticky="nsew")
-
-        # self.optionmenu_1 = customtkinter.CTkOptionMenu(self.tabview.tab("CTkTabview"), dynamic_resizing=False,
-        #                                                values=["Value 1", "Value 2", "Value Long Long Long"])
-        # self.optionmenu_1.grid(row=0, column=0, padx=20, pady=(20, 10))
-        # self.combobox_1 = customtkinter.CTkComboBox(self.tabview.tab("CTkTabview"),
-        #                                            values=["Value 1", "Value 2", "Value Long....."])
-        # self.combobox_1.grid(row=1, column=0, padx=20, pady=(10, 10))
+        self.appearance_mode_label = customtkinter.CTkLabel(self.tabview.tab("Main"), text="Logged in as:")
+        self.appearance_mode_label.grid(row=0, column=10)
+        self.appearance_mode_label = customtkinter.CTkLabel(self.tabview.tab("Main"), text=dict_user["999999"])  # below
+        self.appearance_mode_label.grid(row=1, column=10)  # , padx=0, pady=0 logged in as dict entry
 
         progressbar = customtkinter.CTkProgressBar(master=self.tabview.tab("Main"),
                                                    width=160,
@@ -95,84 +123,35 @@ class App(customtkinter.CTk):
         progressbar.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
         value = 0.1
         progressbar.set(value)
-        # self.label_tab_2 = customtkinter.CTkLabel(self.tabview.tab("Tab 2"), text="CTkLabel on Tab 2")
-        # self.label_tab_2.grid(row=0, column=0, padx=20, pady=20)
 
-    #
-    ## create radiobutton frame
-    # self.radiobutton_frame = customtkinter.CTkFrame(self)
-    # self.radiobutton_frame.grid(row=0, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
-    # self.radio_var = tkinter.IntVar(value=0)
-    # self.label_radio_group = customtkinter.CTkLabel(master=self.radiobutton_frame, text="CTkRadioButton Group:")
-    # self.label_radio_group.grid(row=0, column=2, columnspan=1, padx=10, pady=10, sticky="")
-    # self.radio_button_1 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var,
-    #                                                   value=0)
-    # self.radio_button_1.grid(row=1, column=2, pady=10, padx=20, sticky="n")
-    # self.radio_button_2 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var,
-    #                                                   value=1)
-    # self.radio_button_2.grid(row=2, column=2, pady=10, padx=20, sticky="n")
-    # self.radio_button_3 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var,
-    #                                                   value=2)
-    # self.radio_button_3.grid(row=3, column=2, pady=10, padx=20, sticky="n")
-    #
-    ## create slider and progressbar frame
-    # self.slider_progressbar_frame = customtkinter.CTkFrame(self, fg_color="transparent")
-    # self.slider_progressbar_frame.grid(row=1, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
-    # self.slider_progressbar_frame.grid_columnconfigure(0, weight=1)
-    # self.slider_progressbar_frame.grid_rowconfigure(4, weight=1)
-    # self.seg_button_1 = customtkinter.CTkSegmentedButton(self.slider_progressbar_frame)
-    # self.seg_button_1.grid(row=0, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-    # self.progressbar_1 = customtkinter.CTkProgressBar(self.slider_progressbar_frame)
-    # self.progressbar_1.grid(row=1, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-    # self.progressbar_2 = customtkinter.CTkProgressBar(self.slider_progressbar_frame)
-    # self.progressbar_2.grid(row=2, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-    # self.slider_1 = customtkinter.CTkSlider(self.slider_progressbar_frame, from_=0, to=1, number_of_steps=4)
-    # self.slider_1.grid(row=3, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-    # self.slider_2 = customtkinter.CTkSlider(self.slider_progressbar_frame, orientation="vertical")
-    # self.slider_2.grid(row=0, column=1, rowspan=5, padx=(10, 10), pady=(10, 10), sticky="ns")
-    # self.progressbar_3 = customtkinter.CTkProgressBar(self.slider_progressbar_frame, orientation="vertical")
-    # self.progressbar_3.grid(row=0, column=2, rowspan=5, padx=(10, 20), pady=(10, 10), sticky="ns")
-    #
-    ## create scrollable frame
-    # self.scrollable_frame = customtkinter.CTkScrollableFrame(self, label_text="CTkScrollableFrame")
-    # self.scrollable_frame.grid(row=1, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
-    # self.scrollable_frame.grid_columnconfigure(0, weight=1)
-    # self.scrollable_frame_switches = []
-    # for i in range(100):
-    #    switch = customtkinter.CTkSwitch(master=self.scrollable_frame, text=f"CTkSwitch {i}")
-    #    switch.grid(row=i, column=0, padx=10, pady=(0, 20))
-    #    self.scrollable_frame_switches.append(switch)
-    #
-    ## create checkbox and switch frame
-    # self.checkbox_slider_frame = customtkinter.CTkFrame(self)
-    # self.checkbox_slider_frame.grid(row=1, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
-    # self.checkbox_1 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-    # self.checkbox_1.grid(row=1, column=0, pady=(20, 0), padx=20, sticky="n")
-    # self.checkbox_2 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-    # self.checkbox_2.grid(row=2, column=0, pady=(20, 0), padx=20, sticky="n")
-    # self.checkbox_3 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-    # self.checkbox_3.grid(row=3, column=0, pady=20, padx=20, sticky="n")
-    #
-    ## set default values
-    # self.sidebar_button_3.configure(state="disabled", text="Disabled CTkButton")
-    # self.checkbox_3.configure(state="disabled")
-    # self.checkbox_1.select()
-    # self.scrollable_frame_switches[0].select()
-    # self.scrollable_frame_switches[4].select()
-    # self.radio_button_3.configure(state="disabled")
-    # self.appearance_mode_optionemenu.set("Dark")
-    # self.scaling_optionemenu.set("100%")
-    # self.optionmenu_1.set("CTkOptionmenu")
-    # self.combobox_1.set("CTkComboBox")
-    # self.slider_1.configure(command=self.progressbar_2.set)
-    # self.slider_2.configure(command=self.progressbar_3.set)
-    # self.progressbar_1.configure(mode="indeterminnate")
-    # self.progressbar_1.start()
-    # self.textbox.insert("0.0",
-    #                    "CTkTextbox\n\n" + "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.\n\n" * 20)
-    # self.seg_button_1.configure(values=["CTkSegmentedButton", "Value 2", "Value 3"])
-    # self.seg_button_1.set("Value 2")
-    #
+        self.string_input_button_sign_in = customtkinter.CTkButton(self.tabview.tab("Main"), text="Enter",
+                                                           command=self.sign_in_button_event, anchor="center")
+        self.string_input_button_sign_in.grid(row=7, column=0, padx=(10, 0), pady=(0, 0))
+
+        self.entry_p = customtkinter.CTkEntry(self.tabview.tab("Main"), placeholder_text="P Parameter")
+        self.entry_p.grid(row=1, column=0, columnspan=1, padx=(10, 0), pady=(0, 0))  # , sticky="nsew"
+        self.entry_i = customtkinter.CTkEntry(self.tabview.tab("Main"), placeholder_text="I Parameter")
+        self.entry_i.grid(row=2, column=0, columnspan=1, padx=(10, 0), pady=(5, 0))  # , sticky="nsew"
+        self.entry_d = customtkinter.CTkEntry(self.tabview.tab("Main"), placeholder_text="D Parameter")
+        self.entry_d.grid(row=3, column=0, columnspan=1, padx=(10, 0), pady=(5, 0))  # , sticky="nsew"
+
+        # configuration "settings"-tab
+        self.string_input_button_quit = customtkinter.CTkButton(self.tabview.tab("Settings"), text="Options",
+                                                           command=self.quit(), anchor="center")
+        # self.ok = tkinter.Button(self)
+        # self.ok.pack()
+        # self.ok["text"] = "Beenden"
+        # self.ok["command"] = self.quit
+
+        self.string_input_button_sign_in.grid(row=0, column=0, padx=100, pady=(10, 10))
+
+        # disabling widgets
+        self.entry_p.configure(state="disabled")
+
+    def close_operations(self):
+        # operations to end the GUI to get to the background or to shut down.
+        # Button(self, text="Quit", command=quit).pack()
+        pass
 
     def open_input_dialog_event(self):
         dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
@@ -186,8 +165,7 @@ class App(customtkinter.CTk):
         customtkinter.set_widget_scaling(new_scaling_float)
 
     def sign_in_button_event(self):
-        dict_user = {"Leroy": "Harreh"}
-        print(dict_user[self.entry_user.get()])
+
         try:
             if dict_user[self.entry_user.get()] == self.entry_psw.get() and self.entry_user.get() in dict_user:
                 return True
@@ -196,5 +174,6 @@ class App(customtkinter.CTk):
 
 
 if __name__ == "__main__":
+    global app
     app = App()
     app.mainloop()
